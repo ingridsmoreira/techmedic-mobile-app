c4b0e7d20f5e92eacbec380e445b50a6d38c503a
Ingrid Moreira <93408543+ingridsmoreira@users.noreply.github.com>
2023-06-17 19:52:16 +0100

    proximas consultas

--- src/app/shared/proximas-consultas/proximas-consultas.component.ts
+++ src/app/shared/proximas-consultas/proximas-consultas.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { cardMedico } from 'src/app/core/model/enum/cardMedico';
+import { Especialidades } from 'src/app/core/model/enum/especialidades';
 import { CardMedico } from 'src/app/core/model/interfaces/medico.interface';
 
 @Component({
@@ -7,5 +9,29 @@ import { CardMedico } from 'src/app/core/model/interfaces/medico.interface';
   styleUrls: ['./proximas-consultas.component.sass'],
 })
 export class ProximasConsultasComponent {
-  consultas: CardMedico[] = [];
+  dummyCardMedico: CardMedico[] = [
+    {
+      medicoId: 1,
+      medicoSexo: 'M',
+      medicoNome: 'Fulano de Tal',
+      medicoEspecialidade: Especialidades.Geral,
+      medicoPhotoUrl:
+        'https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg?w=996&t=st=1687023610~exp=1687024210~hmac=b8bd5979930214f754b2250664900f96b6a0125107c2836a2a341350be013487',
+      data: new Date(),
+      hora: '08:00',
+      tipo: cardMedico.calendario,
+    },
+    {
+      medicoId: 2,
+      medicoSexo: 'F',
+      medicoNome: 'testando de Tal',
+      medicoEspecialidade: Especialidades.Pediatra,
+      medicoPhotoUrl:
+        'https://img.freepik.com/free-photo/woman-doctor-wearing-lab-coat-with-stethoscope-isolated_1303-29791.jpg?w=996&t=st=1687023610~exp=1687024210~hmac=b8bd5979930214f754b2250664900f96b6a0125107c2836a2a341350be013487',
+      data: new Date(),
+      hora: '09:00',
+      tipo: cardMedico.calendario,
+    },
+  ];
+  @Input() consultas: CardMedico[] = this.dummyCardMedico;
 }
